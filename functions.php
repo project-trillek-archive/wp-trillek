2a254bbea7c805b211d6646c6a337479972ada75
Oliver Caldwell <olliec87@gmail.com>
2013-08-16 23:35:14 +0100

    Added a hook to clean up wp_head.

--- functions.php
+++ functions.php
@@ -1,1 +1,26 @@
 <?php
+
+/**
+ * Cleans up the WordPress header. By default it outputs a lot of rubbish.
+ */
+function clean_wp_head_output()
+{
+	if (!is_admin())
+	{
+		remove_action('wp_head', 'rsd_link');
+		remove_action('wp_head', 'wlwmanifest_link');
+		remove_action('wp_head', 'index_rel_link');
+		remove_action('wp_head', 'wp_generator');
+		remove_action('wp_head', 'rel_canonical');
+		remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+		remove_action('wp_head', 'index_rel_link');
+		remove_action('wp_head', 'feed_links_extra', 3);
+		remove_action('wp_head', 'feed_links', 2);
+		remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+		remove_action('wp_head', 'start_post_rel_link', 10, 0);
+		remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+		wp_deregister_script('l10n');
+	}
+}
+
+add_action('init', 'clean_wp_head_output');
