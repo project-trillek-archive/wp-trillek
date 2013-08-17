602701ced3bec00a31fd971ed5fc508247b642b9
Oliver Caldwell <olliec87@gmail.com>
2013-08-17 18:17:49 +0100

    Added some basic hooks to the index.

--- page.php
+++ page.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
-A page.
+<?php
+if (have_posts()):
+	while (have_posts()):
+		the_post();
+		the_content();
+	endwhile;
+else:
+?>
+<p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
 <?php get_footer(); ?>
