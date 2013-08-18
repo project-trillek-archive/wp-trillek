7ff192957c2a762166d2d5c6fdf26f03aee20e0a
Oliver Caldwell <olliec87@gmail.com>
2013-08-18 17:45:54 +0100

    Cleaned up the 404 page.

--- 404.php
+++ 404.php
@@ -1,3 +1,15 @@
 <?php get_header(); ?>
-No file for you!
+
+<div <?php post_class(); ?>>
+	<div class='post-head'>
+		<div class='post-title'>
+			<h2>Error 404 - Not found</h2>
+		</div>
+	</div>
+
+	<div class='content-text'>
+		<p>The page you requested could not be found. Please check the URL for any mistakes. Maybe the search box will be able to help?</p>
+	</div>
+</div>
+
 <?php get_footer(); ?>
