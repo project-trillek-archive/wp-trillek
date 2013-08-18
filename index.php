a423f821369ab69199331b751894525038ac054a
Oliver Caldwell <olliec87@gmail.com>
2013-08-18 17:36:41 +0100

    Highlighted sticky posts.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
 
-<div class='post'>
+<div <?php post_class(); ?>>
 	<div class='post-head'>
 		<div class='post-title'>
 			<h2><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h2>
