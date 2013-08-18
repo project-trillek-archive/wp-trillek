985319b02bc3b10e3fa7a4af2fee593fe2587dd5
Oliver Caldwell <olliec87@gmail.com>
2013-08-18 13:10:26 +0100

    Added the previous search query to the search box.

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
 <form class='search-form' role='search' method='get' action='<?php echo home_url( '/' ); ?>'>
-	<input class='text-input' type='text' name='s' placeholder='Search...'>
+	<input class='text-input' value='<?php echo get_search_query(); ?>' type='text' name='s' placeholder='Search...'>
 </form>
