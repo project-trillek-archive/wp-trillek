2f4fb6dd5395d1a0c486dd3fe07f23de9343f196
Oliver Caldwell <olliec87@gmail.com>
2013-08-18 11:56:57 +0100

    Added basic pagination at the bottom of the page.

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php get_header(); ?>
+
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
+
 <h2><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h2>
 <?php the_content(); ?>
+
 <?php endwhile; else: ?>
 <p>No posts match your criteria.</p>
 <?php endif; ?>
+
+<div class='pagination'>
+	<div class='previous-page'><?php previous_posts_link(); ?></div>
+	<div class='next-page'><?php next_posts_link(); ?></div>
+</div>
+
 <?php get_footer(); ?>
