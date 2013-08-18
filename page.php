ce484762a157c85e3614254a40acdad1683473a5
Oliver Caldwell <olliec87@gmail.com>
2013-08-18 17:42:28 +0100

    Made pages look a lot better.
    Now using the styling from posts.

--- page.php
+++ page.php
@@ -1,12 +1,23 @@
 <?php get_header(); ?>
-<?php
-if (have_posts()):
-	while (have_posts()):
-		the_post();
-		the_content();
-	endwhile;
-else:
-?>
-<p>Sorry, no posts matched your criteria.</p>
+
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
+
+<div <?php post_class(); ?>>
+	<div class='post-head'>
+		<div class='post-title'>
+			<h2><?php the_title(); ?></h2>
+		</div>
+	</div>
+
+	<div class='content-text'>
+		<?php the_content(); ?>
+	</div>
+</div>
+
+<?php endwhile; else: ?>
+<div class='content-text'>
+	<p>No posts match your criteria.</p>
+</div>
 <?php endif; ?>
+
 <?php get_footer(); ?>
